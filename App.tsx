b24487d071e58e1825dbdacf531eabe5bff60e3b
mamerida <mamerida2018@gmail.com>
2023-09-06 17:10:27 -0300

    Hellow world and init project

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () =>{
+  return(
+    <View style={{
+      flex:1,
+      justifyContent:"center",
+
+    }}>
+        <Text 
+          style={{
+            fontSize:55,
+            textAlign:"center"
+        }}
+        >
+          Hola Mundo
+        </Text>
+    </View>
+  )
+}
+
+export default App
