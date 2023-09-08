cc199a695804ca0661268586b9e66c42fbb79ede
mamerida <mamerida2018@gmail.com>
2023-09-08 18:46:56 -0300

    conditional render with Platform

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native'
+import { Platform, StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native'
 
 //creo una interface para poder recibir props 
 
@@ -10,24 +10,46 @@ interface Props {
 }
 
 export const Fab = ({title, onPress, position = "br"} : Props) => {
-  return (
-    <View
-        style={[
-            style.fabLocation,
-            (position === 'bl' ? style.left : style.rigth)
-        ]}
-    >
-    <TouchableNativeFeedback 
-        onPress={onPress} // este componente no respeta estilos se debe crear un
-                          // view para poder guardar los estilos
-        background={TouchableNativeFeedback.Ripple('#28425B',false,30)}
-    >
-        <View style={style.fab}>
-        <Text style={style.fabText}>{title}</Text>
+    //Como el codigo no se ve  bien en ios debo separar la implementacion
+
+    const ios = () =>{
+        return(
+        <TouchableOpacity
+            activeOpacity={0.5}
+            style={[
+                style.fabLocation,
+                (position === 'bl' ? style.left : style.rigth)
+            ]}
+        >
+            <View style={style.fab}>
+                <Text style={style.fabText}>{title}</Text>
+            </View>
+        </TouchableOpacity>
+        )
+    }
+
+    const android = () =>{
+        return(
+        <View
+            style={[
+                style.fabLocation,
+                (position === 'bl' ? style.left : style.rigth)
+            ]}
+        >
+            <TouchableNativeFeedback 
+                onPress={onPress} // este componente no respeta estilos se debe crear un
+                                    // view para poder guardar los estilos
+                background={TouchableNativeFeedback.Ripple('#28425B',false,30)}
+            >
+                <View style={style.fab}>
+                    <Text style={style.fabText}>{title}</Text>
+                </View>
+            </TouchableNativeFeedback>
         </View>
-    </TouchableNativeFeedback>
-    </View>
-  )
+        )
+    }
+
+    return (Platform.OS === "ios" ? ios() : android())
 }
 
 const style = StyleSheet.create({
